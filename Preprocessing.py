7c076291104a394208a0aaeecd6b28c899847da9
Portmann Marco Daniel <marco.portmann@bfh.ch>
2024-10-09 21:57:20 +0200

    Preprocessing finished

--- Preprocessing.py
+++ Preprocessing.py
@@ -13,7 +13,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import find_peaks
+import copy
 
 # Constants for the dictionary hashes
 CBG:str = 'cbg'
@@ -21,9 +21,31 @@ TT:str = 'minutes_elapsed'
 SI:str = 'start_indices'
 EI:str = 'end_indices'
 SL:str = 'segment_lengths'
+TRAIN:str = 'train'
+VALIDATE:str = 'validate'
 
 class Preprocessing:
-    def __init__(self, train_path:str, rand_seed:int = 42, debug:bool = True) -> None:
+    def __init__(self, train_path:str|list[str], rand_seed:int=42, thresh_quantile:float=0.8, am_masks:int=3, min_non_nan:int=100, debug:bool=True) -> None:
+        """
+        Preprocessor class handles data loading and preprocessing and delivers uniform size segments with artificially missing data and the groundtruths along with it.
+
+        NOTE: Creating the instance automatically performs the processing.
+
+        ## Example Usage 
+
+        .. code-block:: python
+            preprocessor = Preprocessing()
+            training_data = preprocessor.get_processed_default() # This is the only public function
+
+        @param train_path: Path to the training data folder (grabs CSV format). Can also be a list of paths.
+        @param randSeed: Random seed for reproducibility (default 42).
+        @param thresh_quantile: Quantile threshold for censoring data above this percentile (default 0.8).
+        @param am_masks: Number of random masking operations applied to each segment (Mind the segment size is data driven) (default 3).
+        @param min_non_nan: Amount of samples which border each segment, so each side will have min_non_nan / 2 non nan values (default 100).
+        @param debug: If True will provide some more terminal outputs
+        @return: None. Initializes the object with loaded data and processed results.
+        
+        """
         np.random.seed(rand_seed)
         self._debug_flag:bool = debug
 
@@ -38,26 +60,25 @@ class Preprocessing:
         self._nan_segments:dict  = self._copy_cont_segments(self._nan_segments_loc, self._patient_rawdata_dict, CBG)
 
         # Before changing anything
-        self._print_whole_data_info(plot_hist=False)
+        self._print_whole_data_info(plot_hist=debug)
         self._patient_raw_metadata_dict = self._get_metadata()
-        # self._print_patient_data_info(raw_or_processed='raw')
+        self._print_patient_data_info(raw_or_processed='raw')
 
         """Pre Pre Processing"""
         self._remove_small_data_segments()
         self._resize_segments_uniform()
-        self._control_data_segments:dict = self._data_segments.copy() # Create a copy of all data; later to be the train control
+        self._control_data_segments:dict = copy.deepcopy(self._data_segments) # Create a copy of all data; later to be the train control
 
         """Pre Processing"""
-        self._apply_quantile_cut() 
-        self._apply_random_mask(min_non_nan=100)
-        # Randome Masking
-        # Collect pairs of images and store them       
+        self._apply_quantile_cut(nan_above_quantile=thresh_quantile) 
+        self._apply_random_mask(min_non_nan=min_non_nan, am_masks=am_masks)
+        self._output_dict = self._collect_orig_and_train()     
 
         # Some info after processing
         self._patient_processed_metadata_dict = self._get_metadata()
-        # self._print_patient_data_info(raw_or_processed='processed')
+        self._print_patient_data_info(raw_or_processed='processed')
 
-
+    """UTILITY FUNCTIONS"""
     def _info(self, msg:str) -> None:
         """Info print utility to know which class prints to terminal."""
         print("\033[36m\n[Preprocessor][INFO]: ", "\033[0m\n", msg)
@@ -68,11 +89,17 @@ class Preprocessing:
     def _debug(self, msg:str) -> None:
         if self._debug_flag:
             print("\033[35m\n[Preprocessor][DEBUG]: ", "\033[0m\n", msg)
-
-    def _load_data(self, path:str) -> dict:
+   
+    """PRE PRE PROCESSING"""
+    def _load_data(self, path:str|list[str]) -> dict:
         """Load data as dataframe and return dict with each patient"""
 
-        patient_files = [patient for patient in os.listdir(path) if patient.endswith('.csv')]
+        if isinstance(path, list):
+            patient_files = []
+            for p in path:
+                patient_files.extend([patient for patient in os.listdir(p) if patient.endswith('.csv')])
+        else:
+            patient_files = [patient for patient in os.listdir(path) if patient.endswith('.csv')]
         self._info(f"Loaded {len(patient_files)} files: {patient_files}")
         patient_dict = {}
 
@@ -133,7 +160,8 @@ class Preprocessing:
                 segments[patient].append(np.asarray(dict[patient][col][loc:segments_loc[patient][EI][index]]))
 
         return segments
-    
+   
+    """DEBUGGING / INSIGHTS"""
     def _print_whole_data_info(self, plot_hist:bool = True) -> None:
         """This function prints information about the amount and statistics of continous segments, both nan and non nan."""
         non_nan_total = 0
@@ -176,6 +204,7 @@ class Preprocessing:
         self._debug(msg)
 
     def _print_patient_data_info(self, raw_or_processed:str = 'raw') -> None:
+        """SImilar to print whole data, but the output will be for each patient individually."""
         raw_bool = True if raw_or_processed == 'raw' else False
 
         if raw_bool:
@@ -189,7 +218,7 @@ class Preprocessing:
             self._debug(f"+++ Patient_{key}_{'rawdata' if raw_bool else 'processed'}+++:\n {dat.describe()}\n\n {nan.describe()}\n\n")
 
     def _get_metadata(self) -> dict[pd.DataFrame]:
-        """Similar to print info but this does it for each patient individually. The pd frames are stored for each patient key."""
+        """This functions purpose is mainly to gather data into the metadata structure to print info on it."""
         metadata = {}
         for key in self._patient_rawdata_dict.keys():
             # self._patient_metadata_dict[key] = {}
@@ -204,7 +233,8 @@ class Preprocessing:
             metadata[key]['nan'] = pd.DataFrame(nan_segs_lens, columns=[f"patient_{key}_nan"])
 
         return metadata
-
+   
+    """PRE PROCESSING"""
     def _remove_small_data_segments(self, min_non_nan:int = 100) -> None:
         """For now this removes from the continous data segments all the ones which are shorter than the longest nan segment."""
         for key, segments_list in self._data_segments.items():
@@ -235,17 +265,17 @@ class Preprocessing:
                 self._data_segments[key][i] = seg[:shortest_segment_len]# access the segment and slice from start to len
             
     def _apply_quantile_cut(self, nan_above_quantile:float = 0.8) -> None:
+        """This function evaluates the value for the given quantile from the raw data per patient. For each of the segments of the patient values above are set to nan."""
         for key in self._data_segments.keys():
             thresh = self._patient_rawdata_dict[key][CBG].quantile(nan_above_quantile) # NOTE we are using the complete dataset to define the quantile not each segment!
             msg = f'Patient_{key}: {nan_above_quantile} quantile threshold was {thresh}'
             self._debug(msg)
 
             for i,seg in enumerate(self._data_segments[key]):
-                # print(type(seg), np.quantile(seg, 0.8))
                 self._data_segments[key][i] = np.where(seg > thresh, np.nan, seg)
 
-    def _apply_random_mask(self, min_non_nan:int = 100) -> None:
-        """This function applies a mask to a random valid location in the segment. THe mask size is given by average nan segments length per patient."""
+    def _apply_random_mask(self, min_non_nan:int = 100, am_masks:int = 3) -> None:
+        """This function applies a mask to a random valid location in the segment. The amount of masks is user defined, the size of them data driven."""
         # for loop over patient keys
         for key in self._data_segments.keys():
 
@@ -253,13 +283,13 @@ class Preprocessing:
                 self._warn(f"In apply random mask: No data for key: {key}. Omitting.")
                 continue
 
-            # Get the average nan segment length
+            # Get the average nan segment length for this patient 
             key_nan_lens = np.asarray([len(i) for i in self._nan_segments[key]])
             key_segment_len = len(self._data_segments[key][0])
 
             median_nan_length = np.floor(np.median(key_nan_lens))
             mean_nan_length = np.floor(np.mean(key_nan_lens))
-            combined = (median_nan_length + mean_nan_length) // 2 # I couldnt decide whether mean or median is better so we do both.            
+            combined = int((median_nan_length + mean_nan_length) // 2) # I couldnt decide whether mean or median is better so we do both.            
             self._debug(f"Patient_{key}: [mean, median, combined] nan segment length of [{mean_nan_length},{median_nan_length}, {combined}] and data segment length of {key_segment_len}")
 
             # Check if it is smaller than the segments in question
@@ -268,23 +298,67 @@ class Preprocessing:
                 continue
 
             # Get the valid indices for insertion
+            max_start = key_segment_len - (np.ceil(min_non_nan / 2) + combined)
+            valid_indices = np.arange(np.ceil(min_non_nan / 2), max_start, 1, dtype=int)
 
             # at random insert the nan mask
+            for i, __ in enumerate(self._data_segments[key]):
+                idx = np.random.choice(valid_indices, am_masks, replace=False)
+                for j in idx:
+                    self._data_segments[key][i][j:j+combined] = np.nan
+
+    def _collect_orig_and_train(self) -> dict[dict[list[np.ndarray]]]:
+        """This function collects the trainign and validations segments for each patient."""
+        output_dict = {}
+        for key in self._data_segments.keys():
+            output_dict[key] = {}
+            output_dict[key][TRAIN] = self._data_segments[key]
+            output_dict[key][VALIDATE] = self._control_data_segments[key]
+
+        return output_dict
+
+    def get_processed_default(self) -> dict[dict[list[np.ndarray]]]:
+        """
+        This function returns the following datastructure:\n
+        -   A dictionary with numerical keys corresponding to the patients.\n
+        -   At each key there is an inner dict with keys TRAIN and VALIDATE.\n
+        -   The inner dict at TRAIN, VALIDATE contains a list each.\n
+        -   The list contains numpy arrays, these are the segments to train on.\n
+
+        The arrays under TRAIN contain nan values (Via quantile cutoff and random masking -> default).\n
+        The arrays under VALIDATE contain the groundtruth CBG values.
+
+        They are ordered, example:
+        ```
+        processed_dict = preproc.get_processed_default()
+
+        processed_dict[patient][VALIDATE][i]
+        ```
+        corresponds to 
+        ```
+        processed_dict[patient][TRAIN][i] 
+        ```
+        """
+        return self._output_dict
 
 if __name__ == '__main__':
     TRAIN_PATH = 'Data/Ohio2020_processed/train'
     preproc = Preprocessing(TRAIN_PATH)
+    processed_dict = preproc.get_processed_default()
 
-    train_segments = preproc._data_segments
     patient:int = 1
-    plt.figure()
     n_plots = 5
+
+    plt.figure()
+    #plt.title(f"Example data for training, groundtruth and processed for patient {patient}")
     for i in range(5):
         plt.subplot(n_plots, 1, i+1)
-        peaks, __ = find_peaks(train_segments[patient][i], height=np.quantile(train_segments[patient][i], 0.8), distance=50)
-        plt.plot(train_segments[patient][i])
-        plt.scatter(peaks, train_segments[patient][i][peaks])
+        plt.plot(processed_dict[patient][VALIDATE][i])
+        plt.plot(processed_dict[patient][TRAIN][i])
         plt.grid('on')
+        plt.xlabel('samples')
+        plt.ylabel('CBG')
+        plt.legend(["Groundtruth", "Training"])
 
     plt.tight_layout()
     plt.show()
